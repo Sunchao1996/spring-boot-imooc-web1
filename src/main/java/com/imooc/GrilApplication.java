4fb748477c6658d571dad1fb074e494f5e0dbe38
sunchao <2061857679@qq.com>
2018-01-17 09:55:13 +0800

    Spring-Boot添加使用Servlet

--- src/main/java/com/imooc/GrilApplication.java
+++ src/main/java/com/imooc/GrilApplication.java
@@ -3,8 +3,12 @@ package com.imooc;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+import javax.servlet.annotation.ServletSecurity;
 
 @SpringBootApplication
+@ServletComponentScan
 public class GrilApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/imooc/servlet/ServletBeanConfiguration.java
@@ -0,0 +1,16 @@
+package com.imooc.servlet;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Sunc on 2018/1/17.
+ */
+@Configuration
+public class ServletBeanConfiguration {
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(){
+        return new ServletRegistrationBean(new HelloServlet(),"/hello1");
+    }
+}
